e8f34335815a4ff5bc853cc8d70cd782004d4259
Leon Lau <94111861+nya10@users.noreply.github.com>
2022-01-13 14:37:33 +0700

    Add files via upload

---
+++ Chapter 5/5.1 Randomized Algorithm/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+int main()
+{
+    std::srand ( unsigned ( std::time(0) ) );
+    std::vector<int> vec{1,2,3,4,5,6,7,8,9,10};
+
+    //built in random generator
+    std::random_shuffle( vec.begin(), vec.end() );
+
+    //showing the result
+    for(int i = 0; i < vec.size(); i = i + 1)
+    {
+        std::cout << vec[i] << ' ';
+    }
+
+    return 0;
+}
