3377f1cd04d7ed7873fb4e3a14f81af126816c24
Leon Lau <94111861+nya10@users.noreply.github.com>
2022-01-17 15:07:04 +0700

    Add files via upload

---
+++ Chapter 5/Problem 5.2 RANDOM_SEARCH/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include "find.h"
+#include "random.h"
+
+using namespace std;
+
+int random_search(vector<int> arr, int x)
+{
+    vector<int> index;
+    int size_count = 0;
+    const int arr_size = arr.size();
+
+    while(size_count != arr_size)
+    {
+        int y = random(0,arr_size - 1);
+        if(arr[y] == x)
+        {
+            return y;
+        }
+        else if(find_vector(index,y) == 0)
+        {
+            index.push_back(y);
+            size_count = size_count + 1;
+        }
+        else
+        {
+
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    srand(time(0));
+    vector<int> arr{1,4,7,6,12,14,22,19,18,13};
+    cout << random_search(arr, 3);
+
+    return 0;
+}
