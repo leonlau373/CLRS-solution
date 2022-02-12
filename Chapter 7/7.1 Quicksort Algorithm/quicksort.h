b551d7787d791ac36e91df6248a3bb5e2024a3a8
Leon Lau <94111861+nya10@users.noreply.github.com>
2022-02-12 13:35:19 +0700

    Add files via upload

---
+++ Chapter 7/7.1 Quicksort Algorithm/quicksort.h
@@ -0,0 +1,7 @@
+#ifndef QUICKSORT_H_INCLUDED
+#define QUICKSORT_H_INCLUDED
+
+int partition_quick(std::vector<int>& arr, int beg, int fin);
+void quicksort(std::vector<int>& arr, int beg, int fin);
+
+#endif // QUICKSORT_H_INCLUDED
